a0df49a6479d9869ce340963dd0a442107547db0
ale1900 <alejandrita1900@gmail.com>
2023-02-03 14:22:37 +0100

    Primer commit rama Ale

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import './Main.css';
+import React from "react";
+import "./Main.css";
 
 //Main Ale
 function Main() {
   return (
     <>
-    <div>Main</div>
+      <div className="text-center bg-image">
+        <div className="mask">
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="text-white">
+              <h1 className="mb-3">
+                Recursos para ayudarte a promover <br /> y vender tu <span> Arte</span>
+              </h1>
+              <a className="btn btn-light btn-lg" role="button">
+                Saber más
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Main
+export default Main;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
 //Navbar Ale
 function Navbar() {
   return (
     <>
-    <div>Navbar</div>
+      <nav
+        id="navbar"
+        className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
+      >
+        <div className="container-fluid">
+          <a id="logo" className="navbar-brand"></a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-md-auto gap-2">
+              <li className="nav-item rounded">
+                <a className="nav-link active" aria-current="page">
+                  Inicio
+                </a>
+              </li>
+              <li className="nav-item rounded">
+                <a className="nav-link">Nuestras series</a>
+              </li>
+              <li className="nav-item rounded">
+                <a className="nav-link">Sobre nosotros</a>
+              </li>
+              <li className="nav-item rounded">
+                <a className="nav-link">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
