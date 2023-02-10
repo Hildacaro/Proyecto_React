1ea69e2b2f8a5efedab76a4f845252d039c5c990
Auri <aurigutierrez499@gmail.com>
2023-02-10 10:26:55 +0100

    textos auri

--- src/components/Empresa/empresa-props.jsx
+++ src/components/Empresa/empresa-props.jsx
@@ -7,21 +7,23 @@ function EmpresaProps() {
     <div id="empresa">
       <Empresa
         titulo="Nuestra Empresa"
-        nombre="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+        nombre="En la Galería OGILVY. Madrid. Llevamos casí tres décadas ofreciendo exposiciones de pintura contemporánea de los artistas más relevantes del panorama nacional. La galería fue fundada en el año 1985 y desde entonces se celebran exposiciones mensuales tanto de jóvenes talentos como de pintores consagrados.
+
+        "
         imagen="arbol"
-        descripccion="Last updated 3 mins ago"
+        descripccion="Historia"
       />
       <Empresa
         titulo="Nuestra Misión"
-        nombre="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+        nombre="Conservar la colección para que las futuras generaciones la disfruten. Examinarla regularmente y realizar las labores necesarias de restauración; garantizar el transporte seguro de las obras en caso de préstamo a otras instituciones."
         imagen="caracol"
-        descripccion="Last updated 3 mins ago"
+        descripccion="Objetivos"
       />
       <Empresa
         titulo="Nuestros Valores"
-        nombre="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+        nombre="Olgivy se compromete a buscar la excelencia en todas sus actividades, desde la conservación de las obras hasta su presentación al público; desde la labor educativa hasta la atención al visitante; desde la investigación desarrollada hasta la organización de exposiciones y la publicación de catálogos que sean referentes en el entorno académico. El compromiso con la calidad y el respeto del patrimonio es fundamental para generar confianza entre los visitantes y poder servir cada día a un mayor número de personas"
         imagen="galeria"
-        descripccion="Last updated 3 mins ago"
+        descripccion="Más Valores"
       />
     </div>
   );
